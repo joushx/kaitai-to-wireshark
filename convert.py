a7cd6c67a707cd3ef80edfb76d8be257b37e7333
Johannes Mittendorfer <johannes@johannes-mittendorfer.com>
2022-03-31 10:50:43 +0200

    Clean up code

--- convert.py
+++ convert.py
@@ -9,8 +9,18 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-ksy = load(open(sys.argv[1]).read(), Loader=Loader)
+# load definition file into string
+definition_file = sys.argv[1]
+definition = open(definition_file).read()
+
+# parse definition yaml
+ksy = load(definition, Loader=Loader)
+
+# read template
 template = open("templates/template.lua").read()
 
+# generate output
 template = Template(template,trim_blocks=True,lstrip_blocks=True)
+
+# print result
 print(template.render(data=ksy))
